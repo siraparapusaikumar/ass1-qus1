a9de7dffc676bdf471bb008906083234b4f2216a
siraparapusaikumar <123936587+siraparapusaikumar@users.noreply.github.com>
2023-02-08 14:41:17 +0530

    Add files via upload

---
+++ ass1 qus1.py
@@ -0,0 +1,17 @@
+numbers = [1,2,3,4,5,6,7,8,9] 
+
+even, odd= 0, 0
+
+for i in numbers: 
+
+    if i % 2 == 0: 
+
+        even += 1
+
+    else: 
+
+        odd+= 1          
+
+print("Even : ", even) 
+
+print("Odd : ", odd)
